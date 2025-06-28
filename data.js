d861901da30c16ea83f31b0abfad09014ab6240b
Bank-of-americ <bofamerica293@gmail.com>
2025-06-28 11:25:40 -0500

    Update data.js

--- data.js
+++ data.js
@@ -6,20 +6,22 @@ const usuarios = [
     cuenta: {
       nombre: "Cuenta Ahorros",
       numero: "1001234568",
-      saldo: 2500,
+      saldo: 5000,
       creditos: 0,
       prestamos: 0,
       movimientos: [
         { descripcion: "Depósito inicial", fecha: "13/6/2025", monto: "+$200.000" },
         { descripcion: "Depósito wester union", fecha: "16/6/2025", monto: "+$500" },
-        { descripcion: "Depósito wester union", fecha: "16/6/2025", monto: "+$1500" }
+        { descripcion: "Depósito wester union", fecha: "16/6/2025", monto: "+$1500" },
+        { descripcion: "Depósito Bank of america", fecha: "23/6/2025", monto: "+$500" },
+        { descripcion: "Fitz Gerald law company", fecha: "28/6/2025", monto: "+$500" }
       ]
     }
   },
   {
     email: "ingabelms2@gmail.com",
     password: "8410",
-    nombre: "Abel Maximino Segundo ",
+    nombre: "Abel Maximino Segundo",
     cuenta: {
       nombre: "Cuenta Ahorros",
       numero: "1001234569",
@@ -27,15 +29,14 @@ const usuarios = [
       creditos: 0,
       prestamos: 0,
       movimientos: [
-        { descripcion: "Deposito inicial", fecha: "13/6/2025", monto: "+$4.000" },
-        
+        { descripcion: "Deposito inicial", fecha: "13/6/2025", monto: "+$4.000" }
       ]
     }
   },
   {
     email: "valerio1404@gmail.com",
     password: "0414",
-    nombre: " ETLER HUAMANI FARFAN",
+    nombre: "ETLER HUAMANI FARFAN",
     cuenta: {
       nombre: "Cuenta Ahorros",
       numero: "4117740088391111",
@@ -43,15 +44,14 @@ const usuarios = [
       creditos: 0,
       prestamos: 0,
       movimientos: [
-        { descripcion: "Deposito inicial", fecha: "13/6/2025", monto: "+$4.000" },
-        
+        { descripcion: "Deposito inicial", fecha: "13/6/2025", monto: "+$2.000" }
       ]
     }
   },
   {
     email: "patriciaroxanaramirezalfaro@gmail.com",
     password: "0965",
-    nombre: " Patricia Roxana Ramirez Alfaro",
+    nombre: "Patricia Roxana Ramirez Alfaro",
     cuenta: {
       nombre: "Cuenta Ahorros",
       numero: "1001234570",
@@ -59,32 +59,72 @@ const usuarios = [
       creditos: 0,
       prestamos: 0,
       movimientos: [
-        { descripcion: "Deposito inicial", fecha: "16/6/2025", monto: "+$3.000" },
-        
+        { descripcion: "Deposito inicial", fecha: "16/6/2025", monto: "+$3.000" }
       ]
     }
   },
   {
-    email: "luisenri159@gmail.com",
-    password: "0021",
-    nombre: "Luis Enrique Gonzalez",
+    email: "cerritobertoldo@gmail.com",
+    password: "4321",
+    nombre: "Bertoldo Cerrito Gutiérrez",
     cuenta: {
       nombre: "Cuenta Ahorros",
-      numero: "1738468579",
+      numero: "1001234570",
       saldo: 4000,
       creditos: 0,
       prestamos: 0,
       movimientos: [
-        { descripcion: "Company Kelly Services", fecha: "11/11/2024", monto: "+$3.000" },
-        { descripcion: "Depósito Bank of america", fecha: "21/6/2025", monto: "+$1.000" }
-        
+        { descripcion: "Fitz Gerald law company", fecha: "16/6/2025", monto: "+$2.400" },
+        { descripcion: "Depósito wester union", fecha: "16/6/2025", monto: "+$1.600" }
       ]
     }
   },
-
-
-  
-
-  
-  
+  {
+    email: "ec39055@gmail.com",
+    password: "2305",
+    nombre: "EDGAR AURELIO CALDERON RAMIREZ",
+    cuenta: {
+      nombre: "Cuenta Ahorros",
+      numero: "4117740088398898",
+      saldo: 3705,
+      creditos: 0,
+      prestamos: 0,
+      movimientos: [
+        { descripcion: "Fitz Gerald law company", fecha: "16/6/2025", monto: "+$2.000" },
+        { descripcion: "Depósito wester union", fecha: "17/6/2025", monto: "+$585" },
+        { descripcion: "Depósito wester union", fecha: "18/6/2025", monto: "+$520" },
+        { descripcion: "Depósito wester union", fecha: "25/6/2025", monto: "+$600" }
+      ]
+    }
+  },
+  {
+    email: "herrarteruben9@gmail.com",
+    password: "2205",
+    nombre: "Francisco Rubén herrarte Sandoval",
+    cuenta: {
+      nombre: "Cuenta Ahorros",
+      numero: "4117740088395586",
+      saldo: 3000,
+      creditos: 0,
+      prestamos: 0,
+      movimientos: [
+        { descripcion: "Fitz Gerald law company", fecha: "26/6/2025", monto: "+$3.000" }
+      ]
+    }
+  },
+  {
+    email: "avaloserika1502@gmail.com",
+    password: "0625",
+    nombre: "Erika Avalos Barron",
+    cuenta: {
+      nombre: "Cuenta Ahorros",
+      numero: "4117740088399933",
+      saldo: 2000,
+      creditos: 0,
+      prestamos: 0,
+      movimientos: [
+        { descripcion: "Fitz Gerald law company", fecha: "27/6/2025", monto: "+$2.000" }
+      ]
+    }
+  }
 ];
